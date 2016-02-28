d48b92e13d84ae021adf90a0ddf949314d0712fc
lidawn <lidawn1991@gmail.com>
2016-02-28 17:35:59 +0800

    3sum closet

--- 3Sum Closest/Solution.cpp
+++ 3Sum Closest/Solution.cpp
@@ -4,51 +4,23 @@ public:
 
         int i,j,len=nums.size(),k;
         std::sort(nums.begin(),nums.end());
-        int sum=nums[0]+nums[1]+nums[len-1];
+        int sum=0,closet=nums[0]+nums[1]+nums[len-1];
         for(i=0;i<len-2;i++)
         {
             k=i+1;
             j = len-1;
             while(k<j)
             {
-                if(sum == nums[i]+nums[j]+nums[k])
-                {
-                    //
-                    if(sum<target)k++;
-                    else if(sum>target)j--;
-                    else return sum;
-                }
-                else if(sum < nums[i]+nums[j]+nums[k])
-                {
-                    //更大更接近
-                    if(abs(nums[i]+nums[j]+nums[k] - target) < abs(sum-target))
-                    {
-                        sum = nums[i]+nums[j]+nums[k];
-                        k++;
-                    }
-                    else j--;
-                }
-                else
-                {
-                    //更小更接近
-                    if(abs(nums[i]+nums[j]+nums[k] - target) < abs(sum-target))
-                    {
-                        sum = nums[i]+nums[j]+nums[k];
-                        j--;
-                    }
-                    else k++;
-                }
-                /*
-                if((nums[i]+nums[j]+nums[k] - target) >= (sum-target))break;
-                else
-                {
-                    sum = nums[i]+nums[j]+nums[k];
-                    k++;
-                }
-                */
+                sum =nums[i]+nums[j]+nums[k];
+                if(sum == target)return sum;
+                //更接近
+                if(abs(sum - target) < abs(closet-target))closet = sum;
+                if(sum>target)j--;
+                else k++;
+            
                 
             }
         }
-        return sum;
+        return closet;
     }
 };

7f924d90ef3a0b2042ffd0f9e753a33f6b68fbe4
lidawn <lidawn1991@gmail.com>
2016-08-12 16:14:56 +0800

    Binary Tree Paths

--- Binary Tree Paths/Solution.cpp
+++ Binary Tree Paths/Solution.cpp
@@ -9,7 +9,26 @@
  */
 class Solution {
 public:
+    void printPath(TreeNode *node,string s, vector<string> &ret)
+    {
+        if(node)
+        {
+            if(s.empty())
+            {
+                s+=to_string(node->val);
+            }
+            else
+            {
+                s+="->";
+                s+=to_string(node->val);
+            }
+            printPath(node->left,s,ret);
+            printPath(node->right,s,ret);
+            if(node->right==NULL&&node->left==NULL)ret.push_back(s);
+        }
+    }
     vector<string> binaryTreePaths(TreeNode* root) {
+        /*
         vector<string> ret;
         stack<TreeNode*> st;
         if(root)st.push(root);
@@ -23,20 +42,16 @@ public:
             if(ret.size()==0)
             {
                 string s1;
-                s1.append(1,tmp->val+'0');
+                s1=to_string(tmp->val);
                 ret.push_back(s1);
                 it = ret.begin();
             }
             else
             {
-               
-                cout<<ret.size()<<ret[1];
-                 string s2=ret[0];
-                 cout<<s2;
+                string &s2=*it;
                 s2+="->";
-               
-                s2.append(1,tmp->val+'0');
-                *it = s2;
+                s2+=to_string(tmp->val);
+                //*it = s2;
             }
             
             st.pop();
@@ -46,14 +61,21 @@ public:
             if(pushed==2)
             {
                 string s3(*it);
-                if(it+1 == ret.end())ret.push_back(s3);
-                else ret.insert(it+1,s3);
-                cout<<*it;
+                //返回插入元素的迭代器，这里貌似一定要更新这个迭代器
+                it = ret.insert(it+1,s3);
+                it--;
             }
             
             //没有子树，迭代器后移
             else if(pushed==0)it++;
         }
         return ret;
+        */
+        //递归解法
+        vector<string> ret;
+        string s;
+        if(root)printPath(root,s,ret);
+        return ret;
     }
+
 };
